90f4c35b0ca2b26bfa2710c0f37046ff95639aa3
Home <pruthiviraj.rath@mail.com>
2024-12-01 21:59:14 +0530

    changes added

--- src/test/java/com/practice/repo/SpringTestConfiguration.java
+++ src/test/java/com/practice/repo/SpringTestConfiguration.java
@@ -3,8 +3,10 @@ package com.practice.repo;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import io.cucumber.junit.CucumberOptions;
 
 @CucumberContextConfiguration
 @ContextConfiguration(classes = SpringComponentConfiguration.class)
+@CucumberOptions(plugin = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
 public class SpringTestConfiguration extends AbstractTestNGSpringContextTests {
 }
