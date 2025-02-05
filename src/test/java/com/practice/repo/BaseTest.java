b71e3b4a677724082bd49da06005cb77b4af1f28
Home <pruthiviraj.rath@mail.com>
2025-02-05 23:01:42 +0530

    changes added

--- src/test/java/com/practice/repo/BaseTest.java
+++ src/test/java/com/practice/repo/BaseTest.java
@@ -1,15 +1,21 @@
 package com.practice.repo;
 
-import com.codeborne.selenide.WebDriverRunner;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.hasWebDriverStarted;
+import static org.openqa.selenium.Platform.ANDROID;
+
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+import io.qameta.allure.selenide.AllureSelenide;
 
 public class BaseTest extends SpringTestConfiguration implements IRetryAnalyzer {
 
@@ -26,14 +32,15 @@ public class BaseTest extends SpringTestConfiguration implements IRetryAnalyzer 
 
     @AfterMethod
     public void closeDriver() {
-        if (WebDriverRunner.hasWebDriverStarted()) {
-            closeWebDriver();
+        if (hasWebDriverStarted()) {
+            Platform platform = ((RemoteWebDriver) getWebDriver()).getCapabilities().getPlatformName();
+            if (!platform.is(ANDROID)) closeWebDriver();
         }
     }
 
     @Override
     public boolean retry(ITestResult result) {
-        if (!result.isSuccess() && retryCount<2) {
+        if (!result.isSuccess() && retryCount < 2) {
             retryCount++;
             return true;
         } else return false;
--- src/test/java/com/practice/repo/tests/AppiumPractice.java
+++ src/test/java/com/practice/repo/tests/AppiumPractice.java
@@ -1,12 +1,19 @@
 package com.practice.repo.tests;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.clickable;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
-import org.openqa.selenium.By;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import com.practice.repo.BaseTest;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -20,68 +27,61 @@ import java.util.stream.IntStream;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.remote.AutomationName;
 
-public class AppiumPractice {
-
-    AndroidDriver androidDriver;
-    Duration timeout = Duration.ofSeconds(10L);
+public class AppiumPractice extends BaseTest {
 
     @BeforeTest(alwaysRun = true)
     public void setupAndroidDriverWithCapabilities() throws MalformedURLException {
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("appium:platformName", "ANDROID");
+        capabilities.setCapability("appium:platformName", Platform.ANDROID.name());
         capabilities.setCapability("appium:platformVersion", "7");
         capabilities.setCapability("appium:deviceName", "myAndroidVM");
-        capabilities.setCapability("appium:automationName", "UiAutomator2");
-        androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
-        androidDriver.manage().timeouts().implicitlyWait(timeout);
+        capabilities.setCapability("appium:automationName", AutomationName.ANDROID_UIAUTOMATOR2);
+
+        WebDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10L));
+        WebDriverRunner.setWebDriver(driver);
     }
 
     @AfterTest(alwaysRun = true)
     public void teardown() {
+        AndroidDriver androidDriver = (AndroidDriver) getWebDriver();
         androidDriver.pressKey(new KeyEvent(AndroidKey.HOME));
         androidDriver.pressKey(new KeyEvent(AndroidKey.APP_SWITCH));
-        androidDriver.findElement(By.xpath("//android.widget.TextView[@text='CLEAR ALL']")).click();
+        String dismissXpath = "//android.widget.ImageView[contains(@content-desc,'Dismiss')]";
+        String clearAll = "//android.widget.TextView[@text='CLEAR ALL']";
+        SelenideElement dismiss = $x(dismissXpath);
+        if (!$x(clearAll).is(visible)) $$x(dismissXpath).forEach(app -> dismiss.click());
+        else $x(clearAll).click();
     }
 
     @Test
     public void callTest() {
-        androidDriver.findElement(By.xpath("//android.widget.TextView[@content-desc='Apps']")).click();
-        new WebDriverWait(androidDriver, timeout)
-                .until(elementToBeClickable(By.xpath("//android.widget.TextView[@content-desc='Phone']")));
-        androidDriver.findElement(By.xpath("//android.widget.TextView[@content-desc='Phone']")).click();
-        androidDriver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='dial pad']")).click();
-        IntStream.rangeClosed(0, 9)
-                .forEach(num -> androidDriver.findElement(By.xpath(String.format("//android.widget.FrameLayout[contains(@content-desc,'%s')]", num))).click());
-        String dialedNumber = androidDriver.findElement(By.xpath("//android.widget.EditText[contains(@resource-id,'digits')]")).getAttribute("text");
-        assert dialedNumber != null;
-        dialedNumber = dialedNumber.replaceAll("[^0-9]", "");
-        Assert.assertEquals(dialedNumber, "0123456789");
+        $x("//android.widget.TextView[@content-desc='Apps']").click();
+        $x("//android.widget.TextView[@content-desc='Phone']").shouldBe(clickable).click();
+        $x("//android.widget.ImageButton[@content-desc='dial pad']").click();
+        IntStream.rangeClosed(0, 9).forEach(
+                num -> $x(String.format("//android.widget.FrameLayout[contains(@content-desc,'%s')]", num)).click());
+        $x("//android.widget.EditText[contains(@resource-id,'digits')]").shouldHave(attribute("text", "0123456789"));
     }
 
     @Test
     public void scrollTest() {
-        androidDriver.findElement(By.xpath("//android.widget.TextView[@content-desc='Apps']")).click();
-        new WebDriverWait(androidDriver, timeout)
-                .until(elementToBeClickable(By.xpath("//android.widget.TextView[@content-desc='Google']")));
-        androidDriver.findElement(By.xpath("//android.widget.TextView[@content-desc='Google']")).click();
-        new WebDriverWait(androidDriver, timeout)
-                .until(elementToBeClickable(By.xpath("//com.google.android.apps.gsa.searchplate.SearchPlate[contains(@resource-id,'googlequicksearchbox')]")));
-        androidDriver.findElement(By.xpath("//com.google.android.apps.gsa.searchplate.SearchPlate[contains(@resource-id,'googlequicksearchbox')]")).click();
-        new WebDriverWait(androidDriver, timeout)
-                .until(elementToBeClickable(By.xpath("//android.widget.EditText[contains(@resource-id,'googlequicksearchbox')]")));
-        androidDriver.findElement(By.xpath("//android.widget.EditText[contains(@resource-id,'googlequicksearchbox')]")).sendKeys("appium");
+        AndroidDriver androidDriver = (AndroidDriver) getWebDriver();
+        $x("//android.widget.TextView[@content-desc='Apps']").click();
+        $x("//android.widget.TextView[@content-desc='Google']").shouldBe(clickable).click();
+        $x("//com.google.android.apps.gsa.searchplate.SearchPlate[contains(@resource-id,'googlequicksearchbox')]").shouldBe(clickable).click();
+        $x("//android.widget.EditText[contains(@resource-id,'googlequicksearchbox')]").shouldBe(clickable).sendKeys("appium");
         androidDriver.pressKey(new KeyEvent(AndroidKey.ENTER));
-        new WebDriverWait(androidDriver, timeout)
-                .until(visibilityOf(androidDriver.findElement(By.xpath("//android.widget.TextView[@text='IMAGES']"))));
-        Map<String, String> upto = Map.of(
+        $x("//android.widget.TextView[@text='IMAGES']").shouldBe(visible);
+        Map<String, String> scrollUntil = Map.of(
                 "strategy", "-android uiautomator",
-                "selector", "new UiSelector().text(\"Settings\")",
-                "direction", "down"
-        );
-        androidDriver.findElement(By.xpath("//android.view.View[@content-desc='People also ask']")).click();
-        androidDriver.executeScript("mobile: scroll", upto);
-        androidDriver.findElement(By.xpath("//android.view.View[@content-desc='Settings']")).click();
+                "selector", "new UiSelector().text(\"PHONE\")",
+                "direction", "down");
+        androidDriver.executeScript("mobile: scroll", scrollUntil);
+        $x("//android.widget.TextView[@text='PHONE']").click();
     }
 
 }
